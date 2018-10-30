5bcafd8b7b954356ddf6a6893efb5b1a64ccd3af
phipn <gkztest1@gmail.com>
2018-10-30 16:36:55 +0700

    [hotfix] fix pg gem

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BlogApi
+module DatenowCrawler
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -14,7 +14,7 @@ module BlogApi
 
     config.autoload_paths += %W(#{config.root}/lib/meete)
     config.autoload_paths += %W(#{config.root}/app/services)
-
+    config.eager_load_paths << Rails.root.join('lib')
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
