b71fbaa0849fcbb4e373b07507e248288c792058
Phi Pham Nhat <phi.pn@neo-lab.vn>
2018-11-16 15:18:53 +0700

    [hotfix] add lover's avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, :password, presence: true
   validates_uniqueness_of :email
   mount_base64_uploader :avatar, PictureUploader 
+  mount_base64_uploader :lover_avatar, PictureUploader 
 end
